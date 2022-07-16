c499cfab84d12c95bc4fb967596d30a61603105a
NathanL0518 <chenxiliang18@gmail.com>
2022-07-15 20:03:40 -0500

    Updated the length analysis

--- Core_vs_Noncore/scripts/primary_cds.sh
+++ Core_vs_Noncore/scripts/primary_cds.sh
@@ -13,15 +13,20 @@
 set -e
 set -o pipefail
 
-module load python3
-
 # User provided input arguments
-OUT_DIR=/panfs/jay/groups/9/morrellp/liang797/workspace/Cowpea_Pangenome/Core_vs_Noncore/scripts/
-Primary_list=/panfs/jay/groups/9/morrellp/liang797/workspace/Cowpea_Pangenome/Core_vs_Noncore/files/Primary_chro.txt
-GFF3_for_genes=/panfs/jay/groups/9/morrellp/liang797/workspace/Cowpea_Pangenome/Core_vs_Noncore/files/Vunguiculata_IT97K-499-35_v1.2.gene.gff3
+OUT_DIR=/panfs/jay/groups/9/morrellp/liang797/workspace/cowpea/scripts/
+Primary_list=/panfs/jay/groups/9/morrellp/liang797/workspace/cowpea/files/Primary_chro.txt
+GFF3_for_genes=/panfs/jay/groups/9/morrellp/liang797/workspace/cowpea/files/Vunguiculata_IT97K-499-35_v1.2.gene.gff3
 
 #-----------------
 cd ${OUT_DIR}
 
 # For CDS
-python3 subset_GFF_CDS.py ${Primary_list} ${GFF3_for_genes} "/panfs/jay/groups/9/morrellp/liang797/workspace/Cowpea_Pangenome/Core_vs_Noncore/results/Correct_Primary_CDS.bed"
+python3 subset_GFF_CDS.py ${Primary_list} ${GFF3_for_genes} "/panfs/jay/groups/9/morrellp/liang797/workspace/cowpea/results/Correct_Primary_CDS.bed"
+
+/mnt/c/Users/61947/Desktop/Morell Lab/Cowpea_Pangenome/Core_vs_Noncore/files/Primary_chro.txt
+/mnt/c/Users/61947/Desktop/Morell Lab/Cowpea_Pangenome/Core_vs_Noncore/files/Vunguiculata_IT97K-499-35_v1.2.gene.gff3
+/mnt/c/Users/61947/Desktop/Morell Lab/Cowpea_Pangenome/Core_vs_Noncore/results
+/mnt/c/Users/61947/Desktop/Morell Lab/Cowpea_Pangenome/Core_vs_Noncore/results/Correct_Primary_CDS.bed
+
+python3 subset_GFF_CDS.py /mnt/c/Users/61947/Desktop/Morell_Lab/Cowpea_Pangenome/Core_vs_Noncore/files/Primary_chro.txt /mnt/c/Users/61947/Desktop/Morell_Lab/Cowpea_Pangenome/Core_vs_Noncore/files/Vunguiculata_IT97K-499-35_v1.2.gene.gff3 /mnt/c/Users/61947/Desktop/Morell_Lab/Cowpea_Pangenome/Core_vs_Noncore/files/Corrct_Primary_CDS.bed 
