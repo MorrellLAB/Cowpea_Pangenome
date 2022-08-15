a5cb8207757dc998c47d0bf23fa108f05a5e6acc
Peter L. Morrell <pmorrell@umn.edu>
2022-08-15 11:36:19 -0500

    updated figure to pdf

--- VEP_Analysis/vep_plot.py
+++ VEP_Analysis/vep_plot.py
@@ -3,7 +3,6 @@
 #   Plots core and noncore snps and indels from vep results
 #   Elaine Lee - Minneapolis, MN - Apr 3, 2022
 
-
 import pandas as pd
 
 df = pd.DataFrame({'Variant Set': ['SNPs in\nCore', 'SNPs in\nNoncore', 'Indels in\nCore', 'Indels in\nNoncore'], 'Synonymous': [89593, 101751, 0, 0], 'Inframe Indel': [0, 0, 3966, 6526], 'Missense': [81408, 173845, 0, 0], 'Start or Stop Change': [356, 853, 253, 261], 'Stop Gain': [891, 3740, 196, 701], 'Frameshift': [0, 0, 3698, 17267]})
@@ -18,4 +17,4 @@ sns.set_palette("afmhot_r")
 df.set_index('Variant Set').plot(kind='bar', stacked=True, fontsize=18, edgecolor='none')
 plt.legend(fontsize=18)
 plt.xticks(rotation='horizontal')
-plt.savefig('/Users/pmorrell/Desktop/cowpea_vep.png')
+plt.savefig('/Users/pmorrell/Desktop/cowpea_vep.pdf',dpi=300)
